019f64394b3dc422862e330097e3b198a8703973
markmybytes <markmybytes@gmail.com>
2025-02-13 17:34:19 +0800

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(Path(__file__).parent.joinpath('README.md'), encoding='utf-8') as f:
 
 setup(
     name='hketa',
-    version='0.0.4',
+    version='0.0.5',
     description='A Python package that provides normalised data access to public transports in Hong Kong.',
     long_description_content_type='text/markdown',
     long_description=long_description,
