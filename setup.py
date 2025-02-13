dde6bf835574fa1c49d204eefc53abba2ca87843
markmybytes <markmybytes@gmail.com>
2025-02-13 17:34:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+from pathlib import Path
 from setuptools import find_packages, setup
+
+with open(Path(__file__).parent.joinpath('README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='hketa',
     version='0.0.1',
+    description='A Python package that provides normalised data access to public transports in Hong Kong.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url="https://github.com/SuperDumbTM/hketa",
     install_requires=[
         'aiohttp',
         'beautifulsoup4',
